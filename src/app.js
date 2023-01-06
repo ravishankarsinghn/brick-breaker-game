3bd9f4ed9a3d787056c176a4d8261fa56ca1a5f0
Ravi Shankar <ravishankarsinghnissan56378@gmail.com>
2023-01-06 23:53:08 +0530

    write some more code

--- src/app.js
+++ src/app.js
@@ -3,12 +3,10 @@
 const canvas = document.querySelector("#gameScreen");
 const ctx = canvas.getContext("2d");
 
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
+
 // clear canvas drawing
 ctx.clearRect(0, 0, 800, 600);
 
-// drawing rectangle and filling color in created rectangle
-ctx.fillStyle = "#f00";
-ctx.fillRect(20, 20, 100, 100);
-
-ctx.fillStyle = "#0f0";
 ctx.fillRect(201, 200, 50, 50);
